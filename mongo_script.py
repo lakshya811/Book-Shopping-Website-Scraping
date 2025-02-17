b4987747d6f7766c5c8dc48dc603cdae2702214e
Lakshya Kaushik <110414466+lakshya811@users.noreply.github.com>
2025-02-17 18:13:32 +0530

    Update mongo_script.py

--- mongo_script.py
+++ mongo_script.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://lakshyakaushik:Lakshya12345678@cluster0.kllw6.mongodb.net/')
+client = MongoClient("client")
 db = client.scrapy
 collection= db.test_collection
 doc=post={'author' :'Mike',
